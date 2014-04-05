afdaef13a7a12d26af1c053c51d8bb971e994931
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-04-04 23:50:05 -0700

    google image extraction working

--- backend-flask/backend/__init__.py
+++ backend-flask/backend/__init__.py
@@ -7,6 +7,9 @@ Lucas, Kevin, Olivier for the Facebook SoCal regional hackathon.
 from flask import Flask, request, jsonify, abort
 from alchemyapi import AlchemyAPI
 import json
+
+from urllib import FancyURLopener
+import urllib2
 # from redis import Redis
 
 app = Flask(__name__, static_url_path='/static')
@@ -50,22 +53,55 @@ def text_to_keyword(text):
         return ''
 
 
-def keyword_to_image(keyword):
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)\
+    Gecko/20071127 Firefox/2.0.0.11'
+
+
+def keyword_to_images(keyword):
     """
     Uses google image api and our extracted keyword
     to find a related image to our status.
     """
-    return keyword
+    searchTerm = keyword
+    searchTerm = searchTerm.replace(' ', '%20')
+
+    # Start FancyURLopener with defined version
+    # myopener = MyOpener()
+
+    # Notice that the start changes for each iteration in order
+    # to request a new set of images for each loop
+    url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+           'v=1.0&q='+searchTerm+'&start='+str(0)+'&userip=MyIP')
+
+    # print url
+    request_obj = urllib2.Request(url, None, {'Referer': 'testing'})
+    response = urllib2.urlopen(request_obj)
+
+    # Get results using JSON
+    results = json.load(response)
+    data = results['responseData']
+    dataInfo = data['results']
+
+    images = []
+    # Iterate for each result and get unescaped url
+    for myUrl in dataInfo:
+        print myUrl['unescapedUrl']
+        images.append(myUrl['unescapedUrl'])
+        # return myUrl['unescapedUrl']
+        # myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
+    return images
+    # return keyword
 
 
-def text_to_image(text=""):
+def text_to_images(text=""):
     """
     Uses two helper methods to convert text (fb status)
     into a related image
     """
     top_keyword = text_to_keyword(text)
-    img_url = keyword_to_image(top_keyword)
-    return img_url
+    images = keyword_to_images(top_keyword)
+    return images
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -83,8 +119,13 @@ def main():
     # if not text:
     #    abort(404)
 
-    image_url = text_to_image(text)
-    return jsonify({'url': image_url})
+    images = text_to_images(text)
+    return jsonify({
+        'img1': images[0],
+        'img2': images[1],
+        'img3': images[2],
+        'img4': images[3]
+    })
 
 
 app.debug = app.config['DEBUG']
