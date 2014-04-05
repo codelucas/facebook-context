87a63e149116144e873318876ba0619e97eb1ba1
Olivier Truong <olivierxtruong@gmail.com>
2014-04-04 22:28:50 -0700

    fixed event handler

--- frontend-chrome-extension/main.js
+++ frontend-chrome-extension/main.js
@@ -1,11 +1,18 @@
-$formInput = $('.innerWrap [name="xhpc_message_text"]');
+console.log('loaddddddddddddddddddeeeeeeeeeeeeeeddddddddddd-----------');
+$msg = $('.innerWrap textarea[name="xhpc_message"]');
+$msg.on('click', function() {
+  console.log('clicked');
+  setTimeout(function() {
+    $formInput = $('.innerWrap textarea[name="xhpc_message_text"]');
+    $formInput.on('input', function(){
+      console.log('change');
+      /*
+      var text = $formInput.val();
 
-$formInput.on('change', function(){
-  console.log('change');
-  /*
-  var text = $formInput.val();
+      $.post('', text, function(res) {
+        console.log(res);
+      });*/
+    });
+  }, 500);
 
-  $.post('', text, function(res) {
-    console.log(res);
-  });*/
 });
